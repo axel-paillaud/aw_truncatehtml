decd1208a6054bf97eeef94872186e0c37e42588
axel-paillaud <contact@axelweb.fr>
2025-01-31 10:24:39 +0100

    add header stamps

--- aw_truncatehtml.php
+++ aw_truncatehtml.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Copyright since 2024 Axel Paillaud - Axelweb
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to [ton email] so we can send you a copy immediately.
+ *
+ * @author    Axel Paillaud - Axelweb <contact@axelweb.fr>
+ * @copyright Since 2024 Axel Paillaud - Axelweb
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
 if (!defined('_PS_VERSION_')) {
     exit;
 }
